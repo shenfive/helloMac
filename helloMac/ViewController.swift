22123305b9bf41816af58fc6345a0f80e1596be2
Danny Shen <shenfive@gmail.com>
2020-01-04 16:10:43 +0800

    Initial Commit

---
+++ helloMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  helloMac
+//
+//  Created by 申潤五 on 2020/1/4.
+//  Copyright © 2020 申潤五. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
